e7f1b0b40151d3e8fc072bd74e4b43e5b4c0b60e
Kevin Williams <kwilliams9285@gmail.com>
2024-02-26 12:42:14 -0500

    Add Activity History via Account & Date

---
+++ Controllers/ActivityController.cs
@@ -0,0 +1,102 @@
+﻿using QRSPortal2.Models;
+using QRSPortal2.ModelsDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QRSPortal2.Controllers
+{
+    public class ActivityController : Controller
+    {
+        private ApplicationDbContext _db = new ApplicationDbContext();
+
+        // GET: Activity
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Activity/Details/5
+        public ActionResult History(string id, string pd)
+        {
+            DateTime parsedPubDate;
+            List<QRSActivityLog> qRSActivityLogs = new List<QRSActivityLog>();
+
+            if (DateTime.TryParse(pd, out parsedPubDate)) 
+            {
+                qRSActivityLogs = _db.QRSActivityLog.AsNoTracking().Where(x => x.AccountID == id && x.PublicationDate == parsedPubDate).ToList();
+                ViewData["PublicationDate"] = parsedPubDate.ToString("yyyy-MMM-dd");
+                return View(qRSActivityLogs);
+            }
+            return View(qRSActivityLogs);
+        }
+
+        // GET: Activity/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Activity/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Activity/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Activity/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Activity/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Activity/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- ModelsDB/QRSActivityLog.cs
+++ ModelsDB/QRSActivityLog.cs
@@ -11,17 +11,23 @@ namespace QRSPortal2.ModelsDB
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ActivityLogID { get; set; }
+        [Display(Name = "Account ID")]
         public string AccountID { get; set; }
+        [Display(Name = "IP")]
         public string IPAddress { get; set; }
         public string LogInformation { get; set; }
         public string SystemInformation { get; set; }
+        [Display(Name = "User")]
         public string UserName { get; set; }
         public string EmailAddress { get; set; }
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MMM-dd}")]
+        [Display(Name = "PubDate")]
         public DateTime PublicationDate { get; set; }
+        [Display(Name = "RET")]
         public int ReturnAmount { get; set; }
         [StringLength(10)]
         public string Status { get; set; }
+        [Display(Name = "Updated")]
         public DateTime CreatedAt { get; set; }
 
 
