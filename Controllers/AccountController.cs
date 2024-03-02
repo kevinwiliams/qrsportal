dd99a89cbcf0c1c3afaae5ebffa325b6cafb6bac
Kevin Williams <kwilliams9285@gmail.com>
2024-03-02 14:07:59 -0500

    Add validation

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -111,7 +111,7 @@ namespace QRSPortal2.Controllers
                     return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
                 case SignInStatus.Failure:
                 default:
-                    ModelState.AddModelError("", "Invalid login attempt.");
+                    ModelState.AddModelError("", "Incorrect username/password.");
                     return View(model);
             }
         }
