e84bac3a02b7d55b12c4ecfc65f4b9e528a9dab3
Kevin Williams <kwilliams9285@gmail.com>
2024-03-10 08:52:08 -0500

    update report

--- Models/Reports.cs
+++ Models/Reports.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -11,14 +12,24 @@ namespace QRSPortal2.Models
 
     public class SupervisorReport
     {
+        [Display(Name = "Supervisor")]
         public string UserName { get; set; }
+        [Display(Name = "Email")]
         public string EmailAddress { get; set; }
+        [Display(Name = "Account")]
         public string AccountID { get; set; }
+        [Display(Name = "Retailer")]
         public string RetailerName { get; set; }
         public string Company { get; set; }
+        [Display(Name = "Address")]
         public string RetailerAddress { get; set; }
+        [Display(Name = "Pub Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
         public DateTime PublicationDate { get; set; }
+        [Display(Name = "RET")]
         public int TotalReturnAmount { get; set; }
+        [Display(Name = "DRAW")]
         public int TotalDistributionAmount { get; set; }
     }
 }
