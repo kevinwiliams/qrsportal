6d72e666ead9b9ef457ad4465abfd20672fcf8b4
Kevin Williams <kwilliams9285@gmail.com>
2024-03-09 23:44:29 -0500

    Add Report V1

---
+++ Controllers/ReportController.cs
@@ -0,0 +1,115 @@
+﻿using QRSPortal2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QRSPortal2.Controllers
+{
+    [Authorize(Roles = "Admin, Circulation, Supervisor")]
+    public class ReportController : Controller
+    {
+        private ApplicationDbContext _db = new ApplicationDbContext();
+        private AccountController _ac = new AccountController();
+        // GET: Report
+        public ActionResult Supervisor()
+        {
+            try
+            {
+                var sql = @"SELECT [UserName]
+                              ,[EmailAddress]
+                              ,[AccountID]
+                              ,[RetailerName]
+                              ,[Company]
+                              ,[RetailerAddress]
+                              ,[PublicationDate]
+                              ,[TotalReturnAmount]
+                              ,[TotalDistributionAmount]
+                          FROM [QRS_DB].[dbo].[View_Supervisor_Report]";
+
+                var result = _db.Database.SqlQuery<SupervisorReport>(sql).ToList();
+                return View(result);
+
+            }
+            catch (Exception ex)
+            {
+
+                Util.LogError(ex);
+                return View();
+            }
+        }
+
+        // GET: Report/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Report/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Report/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Report/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Report/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Report/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Report/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/Reports.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QRSPortal2.Models
+{
+    public class Reports
+    {
+    }
+
+    public class SupervisorReport
+    {
+        public string UserName { get; set; }
+        public string EmailAddress { get; set; }
+        public string AccountID { get; set; }
+        public string RetailerName { get; set; }
+        public string Company { get; set; }
+        public string RetailerAddress { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public int TotalReturnAmount { get; set; }
+        public int TotalDistributionAmount { get; set; }
+    }
+}
